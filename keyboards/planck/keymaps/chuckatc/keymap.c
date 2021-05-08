f6ae2796fb25a987decbf4833c0ea560d1a7e932
Charles Thornhill-Cole <charlestc@gmail.com>
2021-05-08 19:13:21 +1000

    Add backtick on tap left shift for left-handed Cmd-` , Alt-` etc

--- keyboards/planck/keymaps/chuckatc/keymap.c
+++ keyboards/planck/keymaps/chuckatc/keymap.c
@@ -42,6 +42,7 @@ enum planck_keycodes {
 // keyboards/planck/keymaps/am/keymap.c
 #define CTL_ESC CTL_T(KC_ESC)
 #define SFT_ENT SFT_T(KC_ENT)
+#define SFT_GRV SFT_T(KC_GRV)
 
 #define LT_RAISE LT(_RAISE, KC_TAB)
 
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+------+------+------+------+------+------+------+------+------+------+-------|
  * |Ctl/Esc|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "    |
  * |-------+------+------+------+------+------+------+------+------+------+------+-------|
- * | Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/Ent|
+ * |Shift/`|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/Ent|
  * |-------+------+------+------+------+------+------+------+------+------+------+-------|
  * | Mouse | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right  |
  * `-------------------------------------------------------------------------------------'
@@ -61,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     LT_RAISE, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
+    SFT_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
     MOUSE,    KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
