8970662b2c95906c62141b30984ea4212d05e6a3
Charles Thornhill-Cole <charlestc@gmail.com>
2021-06-26 16:30:43 +1000

    Add Exit Plover below -Z

--- keyboards/planck/keymaps/chuckatc/keymap.c
+++ keyboards/planck/keymaps/chuckatc/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      | Exit |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Mouse |      |      |   #  |   A  |   O  |   E  |   U  |   #  |      |      | Exit |
  * `-----------------------------------------------------------------------------------'
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_PLOVER] = LAYOUT_planck_grid(
     XXXXXXX, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST1, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
     XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST2, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EXT_PLV,
     MOUSE,   XXXXXXX, XXXXXXX, STN_NUM, STN_A,   STN_O,   STN_E,   STN_U,   STN_NUM, XXXXXXX, XXXXXXX, EXT_PLV
 ),
 
